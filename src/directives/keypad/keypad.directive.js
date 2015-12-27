8623bac7be40db383910ea1798232d9add232bb1
Todd Kronenberg <toddkronenberg@gmail.com>
2015-12-27 12:17:51 -0500

    Styled up the keypad and displayProblem templates.

--- src/directives/keypad/keypad.directive.js
+++ src/directives/keypad/keypad.directive.js
@@ -21,14 +21,24 @@ function kmKeypad() {
 function kmKeypadCtrl() {
   let vmKeypad = this;
 
-  vmKeypad.answer = '';
+  vmKeypad.answer = '0';
   vmKeypad.updateAnswer = updateAnswer;
-  vmKeypad.backspace = () => vmKeypad.answer = vmKeypad.answer.slice(0,-1);
+  vmKeypad.backspace = backspace;
+  vmKeypad.reset = () => vmKeypad.answer = '0';
 
   function updateAnswer(input) {
-    if (input === '-') vmKeypad.answer = (vmKeypad.answer * -1).toString();
+    if (input === '-' && vmKeypad.answer === '0') vmKeypad.answer = '-'.concat(vmKeypad.answer);
+    else if (input === '-') vmKeypad.answer = (vmKeypad.answer * -1).toString();
+    else if (vmKeypad.answer === '0' && input !== '.') vmKeypad.answer = input.toString();
     else if (input === '.' && vmKeypad.answer.indexOf('.') !== -1) return;
-    else if (input === '.' && vmKeypad.answer === '') vmKeypad.answer += '0.';
+    else if (input === '.' && (vmKeypad.answer === '0' || vmKeypad.answer === '-0')) vmKeypad.answer += '.';
+    else if (vmKeypad.answer[0] === '-' && vmKeypad.answer[1] === '0' && vmKeypad.answer.indexOf('.') === -1) vmKeypad.answer = '-'.concat(input);
     else vmKeypad.answer += input;
+  }
+
+  function backspace() {
+    if (vmKeypad.answer === '0' || vmKeypad.answer === '-1') vmKeypad.answer = '0';
+    else if (vmKeypad.answer.length === 1 || (vmKeypad.answer.length === 2 && vmKeypad.answer[0] === '-')) vmKeypad.answer = '0';
+    else vmKeypad.answer = vmKeypad.answer.slice(0,-1);
   }
 }
