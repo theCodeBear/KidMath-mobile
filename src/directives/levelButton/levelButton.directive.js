786c6581b8e4fc18f341331d984655741c340995
Todd Kronenberg <toddkronenberg@gmail.com>
2015-12-31 11:21:26 -0600

    Finished making level button.

--- src/directives/levelButton/levelButton.directive.js
+++ src/directives/levelButton/levelButton.directive.js
@@ -12,26 +12,38 @@ function kmLevelButton() {
     bindToController: {},
     controller: kmLevelButtonCtrl,
     controllerAs: 'vmLevelButton',
-    link: link,
     templateUrl: 'directives/levelButton/levelButton.template.html'
   };
 }
 
-function link(scope, elem, attrs) {
-  elem.bind('click', (e) => {
-    alert('clicked')
-  });
-}
-
-kmLevelButtonCtrl.$inject = ['$rootScope', '$state'];
-function kmLevelButtonCtrl($rootScope, $state) {
+kmLevelButtonCtrl.$inject = ['$rootScope', '$scope', '$state', '$ionicPopup'];
+function kmLevelButtonCtrl($rootScope, $scope, $state, $ionicPopup) {
   let vmLevelButton = this;
 
   vmLevelButton.stateName = $state.current.name;
   vmLevelButton.level = $state.params.level;
+  vmLevelButton.showPopup = showPopup;
+  let levelButtons = [];
+  for (let i=0; i<7; i++) {
+    levelButtons.push({
+      text: `Level ${i+1}`,
+      type: 'button-positive level-popup-buttons',
+      onTap(e) {
+        $state.go('app.problems', { type: $state.params.type, level: i+1 });
+      }
+    });
+  }
 
   $rootScope.$on('$stateChangeSuccess', (event, toState, toParams) => {
     vmLevelButton.stateName = toState.name;
     vmLevelButton.level = toParams.level;
   });
+
+  function showPopup() {
+    let myPopup = $ionicPopup.show({
+      title: 'Choose Difficulty Level',
+      scope: $scope,
+      buttons: levelButtons
+    });
+  }
 }
